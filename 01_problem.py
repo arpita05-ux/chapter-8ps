db2b78a9d69fcc487fec35e37659057d89b93149
“Arpita <arpitakumari10653@gmail.com>
2025-06-14 23:24:10 +0530

    using functions to find greatest of three numbers

---
+++ 01_problem.py
@@ -0,0 +1,13 @@
+a = 8
+b = 7
+c = 90
+
+def greatest(a, b, c):
+    if(a>b and a>c):
+        return a
+    elif(b>a and b>c):
+        return b
+    elif(c>b and c>a):
+        return c
+
+print(greatest(a, b, c))
